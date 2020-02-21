dc9007cfc300da70845b0453c72cd72d7fdf2092
ShinobiSondre <48123393+ShinobiSondre@users.noreply.github.com>
2020-02-21 01:05:26 +0100

    Background Blue Removal

--- src/Components/container2.jsx
+++ src/Components/container2.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 import MediaCard from "./cards";
-import {Card} from "@material-ui/core";
+import { Card } from "@material-ui/core";
 import AutoGrid from "./autogrid";
 
 function SimpleContainer2() {
-    return (
-        <React.Fragment>
-            <CssBaseline />
-            <Container maxWidth="max">
-                <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '89vh' }} >
-                    <AutoGrid>
-                    </AutoGrid>
-                </Typography>
-
-            </Container>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container maxWidth="max">
+        <Typography component="div">
+          <AutoGrid></AutoGrid>
+        </Typography>
+      </Container>
+    </React.Fragment>
+  );
 }
 
 export default SimpleContainer2;
